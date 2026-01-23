e5710acb93359ad8969640fb8445fd60b430c12e
khanhnkq <khanhnkq.24itb@vku.udn.vn>
2026-01-23 15:40:42 +0700

    ok

--- src/main/java/org/example/connectcg_be/service/ReportService.java
+++ src/main/java/org/example/connectcg_be/service/ReportService.java
@@ -1,12 +1,12 @@
-package org.example.connectcg_be.service;
-
-import org.example.connectcg_be.entity.Report;
-
-import java.util.List;
-
-public interface ReportService {
-    void createReport(ReportRequest request, String username);
-    List<Report> getReportsByStatus(String status);
-    Report getReportById(Integer id);
-    void updateReport(Integer id, ReportAdminUpdateRequest request, String adminUsername);
-}
+//package org.example.connectcg_be.service;
+//
+//import org.example.connectcg_be.entity.Report;
+//
+//import java.util.List;
+//
+//public interface ReportService {
+//    void createReport(ReportRequest request, String username);
+//    List<Report> getReportsByStatus(String status);
+//    Report getReportById(Integer id);
+//    void updateReport(Integer id, ReportAdminUpdateRequest request, String adminUsername);
+//}
