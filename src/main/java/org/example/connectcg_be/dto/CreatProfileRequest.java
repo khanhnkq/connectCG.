5e238cb64976e9e46b3e7e128fab37e190a5054c
khanhnkq <khanhnkq.24itb@vku.udn.vn>
2026-01-26 13:52:30 +0700

    register
    # Conflicts:
    #	src/main/java/org/example/connectcg_be/repository/UserProfileRepository.java
    #	src/main/resources/application.properties

---
+++ src/main/java/org/example/connectcg_be/dto/CreatProfileRequest.java
@@ -0,0 +1,19 @@
+package org.example.connectcg_be.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class CreatProfileRequest {
+    private String fullName;
+    private String dateOfBirth; // yyyy-MM-dd
+    private String gender;
+    private String maritalStatus;
+    private String purpose; // looking_for
+    private String occupation;
+    private String cityId;
+    private List<Integer> hobbyIds; // [NEW] Danh sách ID sở thích
+    private String avatarUrl;
+    private String bio;
+}
